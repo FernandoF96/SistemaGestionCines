5db26af2ff571bc5b1e57f046c9fa886666ec98d
Sin Nombres Sin Apellidos <Nando_ax5@live.com>
2021-10-07 03:54:35 -0500

    Modifiqué el diseño de la ventana Principal

--- GestionCinesView/frmPrincipal.h
+++ GestionCinesView/frmPrincipal.h
@@ -34,6 +34,15 @@ namespace GestionCinesView {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::ComboBox^ comboBox1;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	protected:
 
 	private:
 		/// <summary>
@@ -48,21 +57,112 @@ namespace GestionCinesView {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
+			// 
+			// comboBox1
+			// 
+			this->comboBox1->FormattingEnabled = true;
+			this->comboBox1->Items->AddRange(gcnew cli::array< System::Object^  >(3) { L"Cliente", L"Trabajador", L"Administrador" });
+			this->comboBox1->Location = System::Drawing::Point(393, 133);
+			this->comboBox1->Name = L"comboBox1";
+			this->comboBox1->Size = System::Drawing::Size(126, 21);
+			this->comboBox1->TabIndex = 0;
+			this->comboBox1->SelectedIndexChanged += gcnew System::EventHandler(this, &frmPrincipal::comboBox1_SelectedIndexChanged);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(320, 136);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(73, 13);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"Tipo Usuario :";
+			this->label1->Click += gcnew System::EventHandler(this, &frmPrincipal::label1_Click);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(394, 206);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(125, 20);
+			this->textBox1->TabIndex = 2;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(326, 209);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(67, 13);
+			this->label2->TabIndex = 3;
+			this->label2->Text = L"Contraseña :";
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(394, 171);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(126, 20);
+			this->textBox2->TabIndex = 4;
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(352, 174);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(32, 13);
+			this->label3->TabIndex = 5;
+			this->label3->Text = L"DNI :";
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(404, 247);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(96, 33);
+			this->button1->TabIndex = 6;
+			this->button1->Text = L"Iniciar Sesion";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(521, 247);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(96, 33);
+			this->button2->TabIndex = 7;
+			this->button2->Text = L"Registrarse";
+			this->button2->UseVisualStyleBackColor = true;
 			// 
 			// frmPrincipal
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(938, 447);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->comboBox1);
 			this->Name = L"frmPrincipal";
 			this->Text = L"frmPrincipal";
 			this->Load += gcnew System::EventHandler(this, &frmPrincipal::frmPrincipal_Load);
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
 	private: System::Void frmPrincipal_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void comboBox1_SelectedIndexChanged(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
 	}
 	};
 }
